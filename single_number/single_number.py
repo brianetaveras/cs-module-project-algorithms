1a448e0c117bf2cd626768aec0b594fac0559ef4
brianetaveras <taveras574@gmail.com>
2020-09-10 20:00:24 -0400

    Finished single number

--- single_number/single_number.py
+++ single_number/single_number.py
@@ -4,12 +4,17 @@ Returns: an integer
 '''
 def single_number(arr):
     # Your code here
-
-    pass
-
+    duplicates = []
+    for i in arr:
+        if i not in duplicates:
+            duplicates.append(i)
+        else:
+            duplicates.remove(i)
+    return duplicates[0]
+        
 
 if __name__ == '__main__':
     # Use the main function to test your implementation
-    arr = [1, 1, 4, 4, 5, 5, 3, 3, 9, 0, 0]
+    arr = [1, 1, 4, 4, 5, 5, 3, 3,8, 8, 9, 0, 0, 7,7]
 
     print(f"The odd-number-out is {single_number(arr)}")
